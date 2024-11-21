1739ffc8312e91e6d16d84903d2a2f657b336a71
Dia505 <dia48261@gmail.com>
2024-11-21 11:58:16 +0545

    Abstraction

---
+++ oop_in_dart/lib/abstraction.dart
@@ -0,0 +1,39 @@
+abstract class Shape {
+  String name;
+  Shape(this.name);
+
+  double calculateArea();
+
+  void displayArea() {
+    print("The area of $name is ${calculateArea()}");
+  }
+}
+
+class Circle extends Shape {
+  double radius;
+  Circle(this.radius) : super("Circle");
+
+  @override
+  double calculateArea() {
+    return 3.14159 * radius * radius;
+  }
+}
+
+class Triangle extends Shape {
+  double base;
+  double height;
+  Triangle(this.base, this.height) : super("Triangle");
+
+  @override
+  double calculateArea() {
+    return 0.5 * base * height;
+  }
+}
+
+void main() {
+  Circle c = new Circle(7.5);
+  Triangle t = new Triangle(3.2, 8.6);
+
+  c.displayArea();
+  t.displayArea();
+} 
